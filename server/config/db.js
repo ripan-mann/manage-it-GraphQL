aff28153d2b9cc6e73c506467f6d50fbc7d32848
ripan-mann <ripanmann3@gmail.com>
2023-02-13 20:55:22 -0800

    Mongo DB init

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const colors = require("colors");
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+
+  console.log(
+    `MongoDB Connectred: ${conn.connection.host}`.cyan.underline.bold
+  );
+};
+
+module.exports = connectDB;
