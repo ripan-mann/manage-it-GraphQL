ecd0faaa1fbd8cbc28c3a45190f21ffd132b939a
ripan-mann <ripanmann3@gmail.com>
2023-02-15 16:09:22 -0800

    Client side Init & Components

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema/schema");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
@@ -8,7 +9,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 connectDB();
-
+app.use(cors());
 app.use(
   "/graphql",
   graphqlHTTP({
